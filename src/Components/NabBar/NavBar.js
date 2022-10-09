f857266f4181fd86d63e08ed4ca4adcd4ffb108a
ErnestStomp <ernesto.lopez.lbm@outlook.com>
2022-10-08 22:07:03 -0500

    NavBar

--- src/Components/NabBar/NavBar.js
+++ src/Components/NabBar/NavBar.js
@@ -2,36 +2,33 @@ import React from 'react'
 import  './NabBar.css'
 const NavBar = () => {
   return (
-    <div >
-        <div className='Nav-Blur'></div>
-        <nav className="navbar navbar-expand-sm navbar-dark  gradient-custom__nav" id="navbar">
-            <div className="container-fluid">  
-                <div className='Nav-Left'>
-                    <div className='Nav-Name navbar-brand'>Ariadna</div>
-                    <span></span>
-                </div>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className='Nav-Right collapse navbar-collapse Nav-List'  id='navbarToggleExternalContent'>
-                    <ul className='navbar-nav Nav-List-Style'>
-                        <li className=' nav-item'>Home</li>
-                        <li className=' nav-item'>Abilities</li>
-                        <li className=' nav-item'>Portfolio</li>
-                        <li className=' nav-item'>About Me</li>   
-                        <button className='Contact-Button' >
+    <nav className="navbar navbar-expand-lg navbar-light ">
+        <div className='Nav-Full container-fluid'>
+            <div className='Nav-Blur'></div>
+            <div className='Nav-Left'>
+                <div className='Nav-Name'>Ariadna</div>
+                <span></span>
+            </div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className=' collapse navbar-collapse Nav-Right' id="navbarNavAltMarkup">
+                <div className='Nav-List'>
+                    <ul className=' navbar-nav Nav-List-Style'>
+                        <li className='nav-item'>Home</li>
+                        <li className='nav-item'>Abilities</li>
+                        <li className='nav-item'>Portfolio</li>
+                        <li className='nav-item'>About Me</li>
+                        <button className='Contact-Button'>
                             Contact
-                        </button>     
+                        </button>
                     </ul>
                     
-                        
                 </div>
-                
-            </div>  
-        </nav>
-    
-    </div>
-
+            </div>
+        
+        </div>
+    </nav>
   )
 }
 
